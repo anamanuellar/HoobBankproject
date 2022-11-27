af79a771a58eb08433beb04b1a31a840ea270e89
Maxwel Fávaro de Oliveira <ana.manuellar@gmail.com>
2022-11-27 11:31:51 -0300

    creating stats and fixing position getstarted btn

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,10 +2,11 @@ import styles from "../style"
 import { discount, robot } from '../assets'
 import GetStarted from './GetStarted'
 
-const Hero = () => (
+const Hero = () => {
+  return (
   <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
     <div className={`flex-1 ${styles.flexStart}
-      flex-col xl:px-0 sm:px-6
+      flex-col xl:px-0 sm:px-16 px-6
       `}>
       <div className='flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2'>
         <img src={discount} alt='discount'
@@ -16,8 +17,8 @@ const Hero = () => (
           <span className="text-white">1 Month</span> Account
         </p>
       </div>
-      <div className="='flex flex-row justify-between items-center w-full">
-        <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
+      <div className="flex flex-row justify-between items-center w-full">
+        <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
           The Next <br className="sm:block hidden" /> {" "}
           <span className="text-gradient">Generation</span> {" "}
 
@@ -27,7 +28,7 @@ const Hero = () => (
         </div>
 
       </div>
-      <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full">
+      <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
         Payment Method.
       </h1>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Our team of experts uses a methodology to identify the credit cards most likely to fit your needs.
@@ -37,7 +38,7 @@ const Hero = () => (
         <img src={robot} alt='billing' className="w-[100%] h-[100%] relative z-[5]" />
 
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient"></div>
-        <div className="absolute z-[1] w-[80%] h-[85%] rounded-full bottom-40 white__gradient"></div>
+        <div className="absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient"></div>
         <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient"></div>
       </div>
       <div className={`ss:hidden ${styles.flexCenter}`}>
@@ -45,7 +46,8 @@ const Hero = () => (
       </div>
 
   </section>
-)
+);
+  };
 
 
 export default Hero
