1f98b47d1f4d6486b9240adb3212be5a583530dd
anamanuellar <ana.manuellar@gmail.com>
2023-04-16 21:47:25 -0300

    alteração deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/HoobBankproject/',
+  base: '/vite-deploy-demo/',
   plugins: [react()]
 })
